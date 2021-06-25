a6f0927341fd3976a78fdc1148b13a03d20f8a06
tingwei628 <sbvq6p199xe@gmail.com>
2021-06-25 23:25:28 +0800

    add snake food

--- src/snake/snake.py
+++ src/snake/snake.py
@@ -40,11 +40,16 @@ def game(stdscr):
     snake_head_dir = curses.KEY_RIGHT
 
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_GREEN)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_RED)
     stdscr.attron(curses.color_pair(1))
-
     for y, x in snake_body:
         stdscr.addstr(y, x, "|")
     stdscr.attroff(curses.color_pair(1))
+
+    stdscr.attron(curses.color_pair(2))
+    snake_food_pos = create_food(snake_body, box_top_left, box_bottom_right)
+    stdscr.addstr(snake_food_pos[0], snake_food_pos[1], "O")
+    stdscr.attroff(curses.color_pair(2))
 
     update_score(len(snake_body), screen_width, stdscr)
 
@@ -97,12 +102,30 @@ def game(stdscr):
             # update_score(key, screen_width, stdscr)
         else:
             continue
-        # stdscr.addstr(snake_head_next[0], snake_head_next[1], "|")
-        # snake_body.append(snake_head_next)
 
-        snake_tail = snake_body[0]
-        stdscr.addstr(snake_tail[0], snake_tail[1], " ")
-        snake_body.pop(0)
+        snake_head = snake_body[-1]
+        if snake_head == snake_food_pos:
+            stdscr.attron(curses.color_pair(2))
+            snake_food_pos = create_food(snake_body, box_top_left, box_bottom_right)
+            stdscr.addstr(snake_food_pos[0], snake_food_pos[1], "O")
+            stdscr.attroff(curses.color_pair(2))
+        else:
+            snake_tail = snake_body[0]
+            stdscr.addstr(snake_tail[0], snake_tail[1], " ")
+            snake_body.pop(0)
+
+
+def create_food(snake_body, box_top_left, box_bottom_right):
+    food = ()  # empty food (y, x)
+    while food == ():
+        food = (
+            randint(box_top_left[0] + 1, box_bottom_right[0] - 1),
+            randint(box_top_left[1] + 1, box_bottom_right[1] - 1),
+        )
+        if food not in snake_body:
+            return food
+        else:
+            food = ()
 
 
 def append_snake(stdscr, snake_body, snake_head_next):
