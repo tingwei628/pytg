8c53401c1332f09baf102b2df46a12d9409b3612
tingwei628 <sbvq6p199xe@gmail.com>
2021-06-26 03:50:52 +0800

    add snake status

--- src/snake/snake.py
+++ src/snake/snake.py
@@ -17,10 +17,16 @@ def game(stdscr):
     # initial setting
     curses.curs_set(0)
     stdscr.nodelay(1)
-    stdscr.timeout(100)
-
+    INITIAL_TIMEOUT = 100
+    snake_timeout = INITIAL_TIMEOUT
+    stdscr.timeout(snake_timeout)
     box_top_left_x = 5
     box_top_left_y = 5
+    KEY_ESC = 27  # exit
+    KEY_SPACE = 32  # pause/resume
+    KEY_ENTER = 10  # reset (restart)
+    START = 1
+    STOP = 0
     screen_height, screen_width = stdscr.getmaxyx()
     screen_height_mid = screen_height // 2
     screen_width_mid = screen_width // 2
@@ -50,11 +56,63 @@ def game(stdscr):
     snake_food_pos = create_food(snake_body, box_top_left, box_bottom_right)
     stdscr.addstr(snake_food_pos[0], snake_food_pos[1], "O")
     stdscr.attroff(curses.color_pair(2))
+    score = 0
+    update_score(score, screen_width_mid, stdscr)
 
-    update_score(len(snake_body), screen_width, stdscr)
+    snake_status = START
+    status_text = "Status: {}".format("START")
+    stdscr.addstr(3, screen_width_mid - len(status_text) // 2, status_text)
 
     while 1:
         key = stdscr.getch()
+
+        if snake_status == START and key == KEY_SPACE:
+            stdscr.nodelay(0)
+            snake_status = STOP
+            status_text = "Status: {}".format("STOP ")
+            stdscr.addstr(3, screen_width_mid - len(status_text) // 2, status_text)
+            continue
+
+        elif snake_status == STOP and key == KEY_SPACE:
+            stdscr.nodelay(1)
+            stdscr.timeout(snake_timeout)
+            snake_status = START
+            status_text = "Status: {}".format("START")
+            stdscr.addstr(3, screen_width_mid - len(status_text) // 2, status_text)
+            continue
+
+        elif key == KEY_ESC:  # exit to menu
+            break
+        elif key == KEY_ENTER:
+            # screen clear
+            stdscr.clear()
+            # timeout
+            stdscr.nodelay(1)
+            snake_timeout = INITIAL_TIMEOUT
+            stdscr.timeout(snake_timeout)
+            # status
+            snake_status = START
+            status_text = "Status: {}".format("START")
+            stdscr.addstr(3, screen_width_mid - len(status_text) // 2, status_text)
+            # snake direction
+            snake_head_dir = curses.KEY_RIGHT
+            # box
+            textpad.rectangle(stdscr, box_top_left[0], box_top_left[1], box_bottom_right[0], box_bottom_right[1])
+            # snake body
+            snake_body = [
+                (screen_height_mid, screen_width_mid - 1),
+                (screen_height_mid, screen_width_mid),
+                (screen_height_mid, screen_width_mid + 1),
+            ]
+            # score
+            score = 0
+            update_score(score, screen_width_mid, stdscr)
+            # food
+            stdscr.attron(curses.color_pair(2))
+            snake_food_pos = create_food(snake_body, box_top_left, box_bottom_right)
+            stdscr.addstr(snake_food_pos[0], snake_food_pos[1], "O")
+            stdscr.attroff(curses.color_pair(2))
+            continue
 
         if key == -1:  # not input
             key = snake_head_dir
@@ -69,7 +127,6 @@ def game(stdscr):
             snake_head_next = (snake_head[0], snake_head_next_x)
             append_snake(stdscr, snake_body, snake_head_next)
             snake_head_dir = key
-            # update_score(key, screen_width, stdscr)
         elif key == curses.KEY_LEFT and snake_head_dir != curses.KEY_RIGHT:
             snake_head_next_x = (
                 (box_bottom_right[1] - snake_step)
@@ -79,7 +136,6 @@ def game(stdscr):
             snake_head_next = (snake_head[0], snake_head_next_x)
             append_snake(stdscr, snake_body, snake_head_next)
             snake_head_dir = key
-            # update_score(key, screen_width, stdscr)
         elif key == curses.KEY_DOWN and snake_head_dir != curses.KEY_UP:
             snake_head_next_y = (
                 (box_top_left[0] + snake_step)
@@ -89,7 +145,6 @@ def game(stdscr):
             snake_head_next = (snake_head_next_y, snake_head[1])
             append_snake(stdscr, snake_body, snake_head_next)
             snake_head_dir = key
-            # update_score(key, screen_width, stdscr)
         elif key == curses.KEY_UP and snake_head_dir != curses.KEY_DOWN:
             snake_head_next_y = (
                 (box_bottom_right[0] - snake_step)
@@ -99,7 +154,6 @@ def game(stdscr):
             snake_head_next = (snake_head_next_y, snake_head[1])
             append_snake(stdscr, snake_body, snake_head_next)
             snake_head_dir = key
-            # update_score(key, screen_width, stdscr)
         else:
             continue
 
@@ -109,6 +163,13 @@ def game(stdscr):
             snake_food_pos = create_food(snake_body, box_top_left, box_bottom_right)
             stdscr.addstr(snake_food_pos[0], snake_food_pos[1], "O")
             stdscr.attroff(curses.color_pair(2))
+            score += 1
+            update_score(score, screen_width_mid, stdscr)
+            # increase speed of snake
+            """
+            snake_timeout = snake_timeout - len(snake_body) % 90
+            stdscr.timeout(snake_timeout)
+            """
         else:
             snake_tail = snake_body[0]
             stdscr.addstr(snake_tail[0], snake_tail[1], " ")
@@ -135,9 +196,9 @@ def append_snake(stdscr, snake_body, snake_head_next):
     stdscr.attroff(curses.color_pair(1))
 
 
-def update_score(score, screen_width, stdscr):
+def update_score(score, screen_width_mid, stdscr):
     score_text = "Score: {}".format(score)
-    stdscr.addstr(1, screen_width // 2 - len(score_text) // 2, score_text)
+    stdscr.addstr(1, screen_width_mid - len(score_text) // 2, score_text)
 
 
 def snake():
