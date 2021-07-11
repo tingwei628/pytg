be0c77087354ac401ec0cbded39aa550894dbcff
tingwei628 <sbvq6p199xe@gmail.com>
2021-07-11 11:18:14 +0800

    add pytest-mock

---
+++ tests/util/test_block.py
@@ -0,0 +1,9 @@
+"""
+test util/block.py
+Test fixure:  stdscr
+assert output
+"""
+
+
+def test_block():
+    pass
---
+++ tests/util/test_menu.py
@@ -0,0 +1,11 @@
+"""
+test util/menu.py
+
+Test fixure:  stdscr
+mock keyboard input
+stub game_entry()
+"""
+
+
+def test_menu():
+    pass
